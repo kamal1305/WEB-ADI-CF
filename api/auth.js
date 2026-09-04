e9771c857ce6266754934baf9caf02d1eee6a849
kamal1305 <kamearcos@gmail.com>
2026-09-05 01:54:13 +0200

    Remove 'user' scope from GitHub OAuth URL

--- api/auth.js
+++ api/auth.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   const clientId = process.env.OAUTH_CLIENT_ID || "Ov23li63sCBWs3RAKxEV";
   const redirectUri = "https://web-adi-cf-d6ap.vercel.app/api/callback";
-  const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=repo,user`;
+  const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=repo`;
   res.redirect(302, url);
 }
