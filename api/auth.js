7e56b26928add906d4c42e3f7f36894e8a51c133
kamal1305 <kamearcos@gmail.com>
2026-09-05 02:01:41 +0200

    Update OAuth scope in auth handler

--- api/auth.js
+++ api/auth.js
@@ -1,6 +1,7 @@
 export default function handler(req, res) {
   const clientId = process.env.OAUTH_CLIENT_ID || "Ov23li63sCBWs3RAKxEV";
   const redirectUri = "https://web-adi-cf-d6ap.vercel.app/api/callback";
-  const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=repo`;
+  const scope = "repo,user,public_repo";
+  const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scope}`;
   res.redirect(302, url);
 }
